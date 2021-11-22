94d2e70555a45393c20c08b31583efa84e964f56
Osipov Sasha <sashaosipov753@gmail.com>
2021-11-22 23:15:37 +0200

    start

---
+++ includes/header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>LetterArea</title>
+</head>
+
+<body>
+
+    <header>
+        <div class="container">
+            <div class="col-2 logo">
+                <img src="/img/logo.png" alt="site logo">
+            </div>
+            <div class="col-8">
+                <nav>
+                    <ul>
+                        <li><a href="/about.php">About</a></li>
+                        <li><a href="/postcards/">Postcards</a></li>
+                        <li><a href="/services/">Services</a></li>
+                        <li><a href="/explore/">Explore</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="col-2">
+                <button class="button">Create an account</button>
+            </div>
+        </div>
+    </header>
+    <main>
